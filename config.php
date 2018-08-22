2aec1e8316dc14b3ccd2141c839fbd2ba6b45608
Bogdan <shekel_ua@i.ua>
2018-08-22 17:20:56 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc_store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc_store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp7/htdocs/oc_store/catalog/');
+define('DIR_SYSTEM', 'C:/xampp7/htdocs/oc_store/system/');
+define('DIR_IMAGE', 'C:/xampp7/htdocs/oc_store/image/');
+define('DIR_LANGUAGE', 'C:/xampp7/htdocs/oc_store/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp7/htdocs/oc_store/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp7/htdocs/oc_store/system/config/');
+define('DIR_CACHE', 'C:/xampp7/htdocs/oc_store/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp7/htdocs/oc_store/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp7/htdocs/oc_store/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp7/htdocs/oc_store/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp7/htdocs/oc_store/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
